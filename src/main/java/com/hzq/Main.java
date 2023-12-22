f15d06b1205b8b70318683261b6b53edaa17d1a2
黄照权 <1462478547@qq.com>
2023-12-22 13:44:06 +0800

    创建项目

---
+++ src/main/java/com/hzq/Main.java
@@ -0,0 +1,12 @@
+package com.hzq;
+
+/**
+ * @author 黄照权
+ * @version 1.0
+ * @date ${DATE}
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
